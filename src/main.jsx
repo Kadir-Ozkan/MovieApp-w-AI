3c9355ec88311f7d54f65361cb6af573f05dfe6d
yavuzkadirozkan <yavuzkadirozkan@gmail.com>
2025-06-10 16:38:24 +0000

    index.html updated

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
 
+const apiKey = import.meta.env.VITE_TMDB_API_KEY;
+const apiUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`;
+
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
