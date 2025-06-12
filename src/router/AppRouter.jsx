5ef9a84eae33da82bc841cc4cfe2c1fd420509b5
Kadir-Ozkan <kadirozkan@yahoo.com>
2025-06-12 19:22:31 +0300

    a

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,24 +1,20 @@
-import Navbar from "../components/Navbar";
-import { Route } from 'react-router-dom';
+import { Route } from "react-router-dom";
 import Main from "../pages/Main";
 import Register from "../pages/Register";
+import Login from "../pages/Login";
+import { Routes } from "react-router-dom";
 
 // toastContainer de Browser da görünsün diye browserRouter sarmalını üst component olan indexedDB.js de yaptık
 const AppRouter = () => {
   return (
     <>
-      <Navbar />
-
       <Routes>
-
         <Route path="/" element={<Main />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-
       </Routes>
-
     </>
-  )
-}
+  );
+};
 
-export default AppRouter
+export default AppRouter;
