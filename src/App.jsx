dd4043f9aceadf440059a2030f785431dbbf5317
yavuzkadirozkan <yavuzkadirozkan@gmail.com>
2025-06-12 14:16:46 +0000

    navbar in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import Router from "./router/Router";
+// src/App.jsx
 import './App.css';
+import Navbar from './components/Navbar';
+import Router from "./router/Router";
 
 function App() {
   return (
     <RecipeProvider>
+      <Navbar />
       <Router />
     </RecipeProvider>
   );
 }
 
-export default App;
