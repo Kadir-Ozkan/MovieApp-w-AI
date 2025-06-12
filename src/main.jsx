4cb16518c72d4d7f9b3d9d9a5e146955eacbead6
Kadir-Ozkan <kadirozkan@yahoo.com>
2025-06-12 19:35:03 +0300

    approuter at main

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-import App from './App.jsx'
+import App from "./App.jsx";
 
 const apiKey = import.meta.env.VITE_TMDB_API_KEY;
 const apiUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`;
 
-
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </BrowserRouter>
+);
